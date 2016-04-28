4a29e14ebec76c8c3b02a5c791635c0f974d11cf
Matt Anderson <matt@mattbanderson.com>
2016-04-27 22:08:15 -0400

    Add layer control. Retrieve brewery data and add as separate layer. Default to only show metroparks. Remove some metroparks-specific text.

--- app/map.js
+++ app/map.js
@@ -3,21 +3,28 @@ var map = L.map('map').setView([39.76, -84.18], 11);
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+var layers = L.control.layers().addTo(map);
 
-addGeoJsonToMap('data/metroparks.geo.json');
+addGeoJsonToMap('data/metroparks.geo.json', 'Metroparks', true);
+addGeoJsonToMap('data/breweries.geo.json', 'Breweries', false);
 
-function addGeoJsonToMap(url) {
+function addGeoJsonToMap(url, layerName, showByDefault) {
   $.get(url, function(data) {
-    L.geoJson(data, {
-      onEachFeature: function (feature, layer) {
-        if (feature.properties && feature.properties.name) {
-          layer.bindPopup(
-            '<h3>' + feature.properties.name + ' Metropark</h3>' +
-            '<p>' + feature.properties.description + '</p>' +
-            '<a href="' + feature.properties.url + '" target="_blank">Learn More</a>'
-          );
+    var layer =
+      L.geoJson(data, {
+        onEachFeature: function (feature, layer) {
+          if (feature.properties && feature.properties.name) {
+            layer.bindPopup(
+              '<h3>' + feature.properties.name + '</h3>' +
+              '<p>' + feature.properties.description + '</p>' +
+              '<a href="' + feature.properties.url + '" target="_blank">More details...</a>'
+            );
+          }
         }
-      }
-    }).addTo(map);
+      });
+    if (showByDefault) {
+      layer.addTo(map);
+    }
+    layers.addOverlay(layer, layerName);
   });
 }
